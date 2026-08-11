50bb4c87603a13d9d71c630e775762d5738157e1
sdhaiddder8 <49123449+sdhaiddder8@users.noreply.github.com>
2026-08-12 00:49:47 +0300

    Initial Commit

---
+++ FlipkartAssignment/FlipkartAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlipkartAssignmentApp.swift
+//  FlipkartAssignment
+//
+//  Created by Syed Danish  on 12/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct FlipkartAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
